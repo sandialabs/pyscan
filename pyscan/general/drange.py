bafe086ebe47fe8f7a1796121221e73b45a8d1ee
Andy Mounce <amounce@sandia.gov>
2023-11-30 10:28:42 -0700

    linted general

--- pyscan/general/drange.py
+++ pyscan/general/drange.py
@@ -13,7 +13,9 @@ def drange(start, delta, stop):
     Returns an array from `start`, with steps of size `delta` to 
     `stop`, inclusive.
 
-    - If ``stop-start`` is not divisible by `delta`, it fits as many integer steps of `delta` as possible, then adds `stop` as the final step.
+    - If ``stop-start`` is not divisible by `delta`,
+      it fits as many integer steps of `delta` as possible, 
+      then adds `stop` as the final step.
     - If ``stop-start < delta`` returns ``[start, stop]``.
     - If ``stop == start`` returns ``[start]``.
 
@@ -35,19 +37,19 @@ def drange(start, delta, stop):
 
     if stop == start:
         return [start]
-    elif np.abs(stop-start) < delta:
+    elif np.abs(stop - start) < delta:
         return [start, stop]
 
-    sign = (stop - start)/np.abs(stop-start) # 1 or -1
+    sign = (stop - start) / np.abs(stop - start)  # 1 or -1
 
-    isdivisible = not ((stop-start)/delta)%1 > 0
+    isdivisible = not ((stop - start) / delta) % 1 > 0
 
     if isdivisible:
         values = np.linspace(start, stop, int(np.abs(stop - start) / (delta) + 1))
     else:
-        n = int(np.floor(np.abs(stop-start)/delta))
+        n = int(np.floor(np.abs(stop - start) / delta))
         
-        values = [start + sign*delta*i for i in range(n+1)]
+        values = [start + sign * delta * i for i in range(n + 1)]
         values += [stop]
 
     return values
