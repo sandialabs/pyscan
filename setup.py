643631540e4b116fe027cc3c7181732dc9515909
Jasmine Mah <jjmah@sandia.gov>
2023-12-07 10:30:33 -0700

    fixed typo

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "drivers",
         "measurement",
         "experiment",
-    ]
+    ],
     url="",
     packages=find_packages(exclude=['docs']),
     long_description=read('README.md'),
