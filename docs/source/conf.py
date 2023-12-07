c9f5da3e2b767cdd02bb2b21b2d2857b1e29bf66
Jasmine Mah <jjmah@sandia.gov>
2023-12-07 10:43:30 -0700

    added requirement numpy to yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Andrew Mounce'
 author = 'Andrew Mounce'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 
