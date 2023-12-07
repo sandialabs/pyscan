70ce5053748b7bb345df55726b3fd37fc99ae347
Ryan Brost <rsbrost@sandia.gov>
2023-12-07 09:58:46 -0700

    modified:   general/test_runinfo.py

--- test/general/test_runinfo.py
+++ test/general/test_runinfo.py
@@ -25,13 +25,21 @@ def test_init_from_noparams():
         assert not init_runinfo.loop2.scan_dict
         assert not init_runinfo.loop3.scan_dict
 
+    checkOne()
+
     def checkTwo():
         assert not init_runinfo.static
         assert not init_runinfo.measured
+        
+    checkTwo()
 
     def checkThree():
         assert not init_runinfo.measure_function
         assert not init_runinfo.trigger_function
 
+    checkThree()
+
     def checkFour():
         assert init_runinfo.initial_pause == 0.1
+
+    checkFour()
