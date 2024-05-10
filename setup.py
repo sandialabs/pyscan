4f6fd99417ed22be9a73ee48fe93e71efbd97d35
Ryan Brost <rsbrost@sandia.gov>
2024-05-09 18:40:18 -0600

    fix: updated setup.py to fix build issue.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import json
 
 def get_version():
     runinfo_dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(runinfo_dir, "../../VERSION.json")
+    path = os.path.join(runinfo_dir, "VERSION.json")
     with open(path) as version_file:
         version = json.load(version_file)['version']
         if type(version) is str:
