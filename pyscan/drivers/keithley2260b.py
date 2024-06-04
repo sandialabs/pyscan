f7319d5891c3204e77e1dbdf44d4f1f0d10cf377
Ryan Brost <rsbrost@sandia.gov>
2024-06-04 13:45:37 -0600

    updated keithley2260b doc string to pass tests.

--- pyscan/drivers/keithley2260b.py
+++ pyscan/drivers/keithley2260b.py
@@ -14,8 +14,9 @@ class Keithley2260B(InstrumentDriver):
         Visa string or an instantiated instrument (return value from
         :func:`.new_instrument`)
 
-    Properties
+    Attributes
     ----------
+    (Properties)
     output_on_delay : float
         Delay before output is turned on [0.00, 99.99]s
     output_off_delay : float
