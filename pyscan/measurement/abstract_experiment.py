c7e17ce765f0357b609860bf265f9f3a35fa594a
Ryan Brost <rsbrost@sandia.gov>
2024-06-16 17:37:43 -0600

    fix(core): added working json converter for runinfo and devices metadata now implemented in abstract experiment's save_metadata method.

--- pyscan/measurement/abstract_experiment.py
+++ pyscan/measurement/abstract_experiment.py
@@ -9,6 +9,7 @@ from pyscan.general import (ItemAttribute,
                             recursive_to_dict,
                             is_list_type)
 from pyscan.measurement.scans import PropertyScan, RepeatScan
+from pyscan.general.json_encoder import CustomJSONEncoder
 
 
 class AbstractExperiment(ItemAttribute):
@@ -226,11 +227,9 @@ class AbstractExperiment(ItemAttribute):
         save_path = self.runinfo.data_path / '{}.hdf5'.format(self.runinfo.long_name)
         save_name = str(save_path.absolute())
 
-        data = recursive_to_dict(self.__dict__)
-
         with h5py.File(save_name, 'a') as f:
-            f.attrs['runinfo'] = json.dumps(data['runinfo'])
-            f.attrs['devices'] = json.dumps(data['devices'])
+            f.attrs['runinfo'] = json.dumps(self.runinfo, cls=CustomJSONEncoder)
+            f.attrs['devices'] = json.dumps(self.devices, cls=CustomJSONEncoder)
 
     def start_thread(self):
         '''Starts experiment as a background thread, this works in conjunction with live plot
