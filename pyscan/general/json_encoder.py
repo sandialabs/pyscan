637045c0b6731f937b56c640af31d27a3aa8995e
rsbrost <151087253+rsbrost@users.noreply.github.com>
2024-06-18 09:54:03 -0600

    Revert "fix(core): corrected the issue plesiopterys identified with saving numpy data types and consolidated the json converter added in the last commit to the pre existing recursive_to_dict function which can now handle numpy data types for saving."
    This reverts commit 18c4baffbb5298539e342fedf872dc19b85b3234.

---
+++ pyscan/general/json_encoder.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """
+    Custom encoder for numpy data types.
+    """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumpyEncoder, self).default(obj)
