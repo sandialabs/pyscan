27ae91c7a1495e05e7887897972ae94ddef886c9
rsbrost <151087253+rsbrost@users.noreply.github.com>
2024-06-18 09:54:03 -0600

    Revert "chore(general): updated the general __init__.py to account for the file changes from previous commits and import successfully with a new order since itemattribute is used in other pyscan/general modules now."
    This reverts commit b71882dacd37290ecb89af88bbf1f96568e65718.

--- pyscan/general/__init__.py
+++ pyscan/general/__init__.py
@@ -1,6 +1,3 @@
-# objects
-from .item_attribute import ItemAttribute
-from .json_encoder import CustomJSONEncoder
 
 # methods
 from .d_range import drange
@@ -8,8 +5,12 @@ from .first_string import first_string
 from .is_list_type import is_list_type
 from .is_numeric_type import is_numeric_type
 from .quadrature_sum import quadrature_sum
+from .recursive_to_dict import recursive_to_dict
+from .recursive_to_item_attribute import recursive_to_itemattribute
 from .same_length import same_length
 from .set_difference import set_difference
 from .stack_or_append import stack_or_append
 from .get_pyscan_version import get_pyscan_version
-from .json_decoder import item_attribute_object_hook
+
+# objects
+from .item_attribute import ItemAttribute
