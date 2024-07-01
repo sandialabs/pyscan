e45deb6cd382147975142ca384b345d5c925bc04
Ryan Brost <rsbrost@sandia.gov>
2024-07-01 12:03:28 -0600

    refactor(drivers)(docs): updated get_resources.py to have more streamlined outputs, organization, and added a doc string. This feature has now been tested to return available connections in a helpful format and has been used to connect with the stanford830 with 2 lines of code replacing 12+.

--- pyscan/general/get_resources.py
+++ pyscan/general/get_resources.py
@@ -2,9 +2,49 @@ from pyvisa import ResourceManager, VisaIOError
 
 
 def get_resources(black_list=[], target=None):
+    """
+    Function that scans for connected VISA resources,
+    optionally filtering out specified resources, and
+    can return a specific target resource or list available resources.
+
+    This function uses the PyVISA library to list all connected instruments that are accessible via VISA.
+    The blacklist is for instruments that the PyVISA resource manager detects but can't connect to/open.
+    The target parameter can be used to search for and return a specific instrument based on a target identifier
+    found either in the resource string or the instrument's identity query response.
+
+    Parameters
+    ----------
+    black_list : (list, optional)
+        A list of resource strings to ignore when listing or searching for resources. Defaults to an empty list.
+    target : (str, optional)
+        A substring to search for within the resource strings or the instrument identity query responses.
+        If specified, the function attempts to find an instrument that matches this target. Defaults to None.
+
+    Returns
+    -------
+    If a target is specified and found, returns the PyVISA resource object
+        corresponding to the target instrument. Otherwise, returns None.
+    If no target is specified, the function does not return a value but
+        prints information about the connected resources.
+
+    Raises
+    ------
+    AssertionError: If a VisaIOError occurs while trying to connect to an instrument it returns an error
+        indicating it might already be connected elsewhere and should be considered diagnostics or blacklisting.
+
+    Note:
+    - It is recommended to specify a non-empty blacklist if there are known resources that should always be ignored
+        to avoid unnecessary connection attempts and errors.
+    """
+
     rm = ResourceManager()
     rs = rm.list_resources()
-    print("Connected resources are: ", rs)
+    list_resources = True
+    if target is not None:
+        list_resources = False
+
+    if list_resources is True:
+        print("Connected resources are: ", rs, "\n")
 
     target_found = False
     for r in rs:
@@ -14,7 +54,8 @@ def get_resources(black_list=[], target=None):
             # Connect to the instrument
             res = rm.open_resource(r)
         except VisaIOError as e:
-            assert False, f"{e}, instrument may already be connected elsewhere. If available or consider blacklisting."
+            err_str = f"{e}, instrument may already be connected elsewhere."
+            assert False, err_str + " If available diagnose the connection or consider blacklisting."
 
         # Check if there is a target and if it is in the resource string
         if (target is not None) and (target in r):
@@ -28,12 +69,13 @@ def get_resources(black_list=[], target=None):
             name = res.query('*IDN?')
             # Check if there is a target resource and whether this is it
             if (target is not None) and (target in name):
-                print(f"Target found in resource {r} with {name}")
+                print(f"Target found in resource {r} with name {name}")
                 # If the target is found break to return the target resource
                 target_found = True
                 break
             # If found, print the resource as well as the I.D.
-            print(r, name)
+            if list_resources is True:
+                print(r, name)
 
         except VisaIOError:
             pass
@@ -42,7 +84,10 @@ def get_resources(black_list=[], target=None):
 
     if target is not None:
         if target_found is True:
-            # print("Returning your target resource.")
+            try:
+                print(f"Returning target resource: {res.query('*IDN?')}")
+            except VisaIOError:
+                pass
             return res
         else:
             print("Target not found, ensure you are trying to access the right resource and that it is available.")
