3502a7df2bf5e1e397e79c9dff983bc4e3b178d4
Jasmine Mah <jjmah@sandia.gov>
2024-08-06 14:57:26 -0600

    feat: Added live_multi_plot function

--- pyscan/plotting/live_plot.py
+++ pyscan/plotting/live_plot.py
@@ -48,3 +48,124 @@ def live_plot(plotting_function, dt=1):
         display.clear_output(wait=True)
 
     return live_plot_function
+
+
+def multi_live_plot(plotting_functions, subplot_locators=None, projections=None, nrows=1, ncols=1,
+                    width_ratios=None, height_ratios=None, subplot_kw=None,
+                    gridspec_kw=None, dt=1):
+    '''
+    Generates a function that executes all plotting_function provided while data
+    is still being taken in Jupyter notebooks. Used by the live plotting
+    functions such as :func:`~pyscan.plotting.basicplots.live_plot2D`.
+
+    Parameters
+    ----------
+    plotting_functions : list
+        list of plotting functions to be refreshed. Each function must take exactly
+        two parameters: expt and ax. The function should take data from expt and plot
+        to a matplotlib axis object.
+    subplot_locators : list, optional
+        list of subplot locators in accordance with matplotlib subplot formatted as
+        A 3-digit integer or a tuple with three numbers (nrows, ncols, index).
+        If omitted, then plotting functions will occupy the axes object according to
+        their index position.
+    projections: list, optional
+        list of projections such as '3d', 'polar', according to each plotting function.
+        If ommitted, all plots default to 2d.
+    nrows : int, optional
+        How many rows to add to the multiplot.
+    ncols : int, optional
+        How many cols to add to the multiplot.
+    width_ratios : array-like of length ncols, optional
+        Defines the relative widths of the columns. Each column gets a relative width of
+        `width_ratios[i] / sum(width_ratios)`. If not given, all columns will have the same
+        width. Equivalent to `gridspec_kw={'width_ratios': [...]}`.
+    height_ratios : array-like of length nrows, optional
+        Defines the relative heights of the rows. Each row gets a relative height of
+        `height_ratios[i] / sum(height_ratios)`. If not given, all rows will have the same
+        height. Convenience for `gridspec_kw={'height_ratios': [...]}`.
+    subplot_kw : dict
+        Dict with keywords passed to matplotlib's `add_subplot` call used to create each subplot.
+    gridspec_kw : dict
+        Dict with keywords passed to matplotlib's `GridSpec` constructor used to create the grid the
+        subplots are placed on.
+    dt : float
+        Time in s between refreshes
+
+    Returns
+    -------
+    func : function
+        Its mandatory parameter is list of experiments in the same order as the plotting_functions as
+        defined in the multi_live_plot function. Keyword arguments can be provided and accessed by the
+        plotting_functions as well to further customize plots.
+
+    '''
+
+    # check if subplot_locators was specified
+    if not subplot_locators:
+        # check if n_cols and n_rows accomodates all of the plotting_functions
+        total_axes = nrows * ncols
+        if len(plotting_functions) > total_axes:
+            raise ValueError(f"not enough plots based on {nrows} nrows and {ncols} ncols to accomodate "
+                             + f"{len(plotting_functions)} plotting_functions.")
+    else:
+        # check plotting functions is same length as subplot locators
+        if not len(plotting_functions) == len(subplot_locators):
+            raise ValueError("subplot_locators must be the same length as plotting_functions")
+
+    # check if projections was specified
+    if projections:
+        # check plotting functions is same length as proejctions
+        if not len(plotting_functions) == len(projections):
+            raise ValueError("projections must be the same length as plotting_functions")
+
+    def live_plot_function(expt, *arg, **kwarg):
+        while (expt.runinfo.running is True and len(expt.runinfo.measured) < 1):
+            sleep(1)
+
+        fig, axs = plt.subplots(nrows, ncols, width_ratios=width_ratios, height_ratios=height_ratios,
+                                subplot_kw=subplot_kw, gridspec_kw=gridspec_kw)
+
+        def plot():
+            if subplot_locators:
+                for i, func in enumerate(plotting_functions):
+                    subplot_locator = subplot_locators[i]
+                    if projections:
+                        projection = projections[i]
+                    else:
+                        projection = None
+                    # check if subplot locator is int or tuple
+                    if isinstance(subplot_locator, int):
+                        ax = plt.subplot(subplot_locator, projection=projection)
+                    elif isinstance(subplot_locator, tuple):
+                        ax = plt.subplot(*subplot_locator, projection=projection)
+                    func(expt, ax, *arg, **kwarg)
+            else:
+                for i, func in enumerate(plotting_functions):
+                    if projections:
+                        projection = projections[i]
+                    else:
+                        projection = None
+                    ax = plt.subplot(nrows, ncols, i + 1)
+                    func(expt, ax, *arg, **kwarg)
+
+        plt.ion()
+
+        while expt.runinfo.running:
+            sleep(dt)
+
+            plt.gca().cla()
+
+            plot()
+
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
+
+        plt.gca().cla()
+
+        plot()
+
+        display.display(plt.gcf())
+        display.clear_output(wait=True)
+
+    return live_plot_function
