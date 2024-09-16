62bd6291857490b6738b7f85e05f035bb1d06467
Ryan Brost <rsbrost@sandia.gov>
2024-09-16 15:57:37 -0600

    fix(install): now install without -e will work since version file moved to pyscan folder. Version workflow set to track accordingly. Install and runinfo will now fail if version not detected.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import json
 
 def get_version():
     runinfo_dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(runinfo_dir, "VERSION.json")
+    path = os.path.join(runinfo_dir, "pyscan/VERSION.json")
     with open(path) as version_file:
         version = json.load(version_file)['version']
         if type(version) is str:
             return 'v' + version
         else:
-            return "no valid version found"
+            assert False, "no valid version found"
 
 
 def read(fname):
