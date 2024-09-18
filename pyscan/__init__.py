645df6e24d717dfb82ac4bad05788eee4d5077f6
Ryan Brost <rsbrost@sandia.gov>
2024-09-18 14:22:54 -0600

    fix: fixing pyscan init to install more robustly.

--- pyscan/__init__.py
+++ pyscan/__init__.py
@@ -1,5 +1,5 @@
+from pyscan.general import *
 from pyscan.measurement import *
 from pyscan.drivers import *
-from pyscan.general import *
 from pyscan.plotting import *
 from pyscan.drivers.testing import *
