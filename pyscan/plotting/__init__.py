3db7893c688647429c2e9d89273ee8c32e39ba3d
Ryan Brost <rsbrost@sandia.gov>
2024-09-26 13:04:45 -0600

    fix: fixing plotting init to account for new multi live plot file.

--- pyscan/plotting/__init__.py
+++ pyscan/plotting/__init__.py
@@ -1,7 +1,7 @@
 # Methods
 from .basic_plots import *
 from .oscilloscope import oscilloscope
-from .live_plot import multi_live_plot
+from .mutli_live_plot import multi_live_plot
 
 # Objects
 from .jupyter_tools import JupyterTools
