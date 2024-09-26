8a121b017db76dac1ef2d596fe365ca17f83dc26
Ryan Brost <rsbrost@sandia.gov>
2024-09-26 12:45:36 -0600

    chore(plotting)(docs): moving plotting dependent docs changes to the plotting PR to group these separately from the main docs changes. Doing this for the get_pyscan_version file this time.

--- pyscan/general/get_pyscan_version.py
+++ pyscan/general/get_pyscan_version.py
@@ -5,7 +5,7 @@ import json
 # function that gets the overarching version of pyscan from VERSION.json
 def get_pyscan_version(path="../VERSION.json"):
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(base_dir, os.pardir, os.pardir, path)
+    path = os.path.join(base_dir, path)
     with open(path) as version_file:
         version = json.load(version_file)['version']
         if type(version) is str:
