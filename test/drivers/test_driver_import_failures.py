3f4a8413cc6083a8302c2200fbebf3b9021ee3df
Mounce <ammounce@gmail.com>
2024-10-04 14:49:38 -0600

    fix imports

--- test/drivers/test_driver_import_failures.py
+++ test/drivers/test_driver_import_failures.py
@@ -1,9 +1,5 @@
 import pytest
 import pyscan as ps
-from pyscan.drivers.heliotis.exceptions import HeliosImportException
-from pyscan.drivers.keysight.exceptions import KeysightSD1Error
-from pyscan.drivers.spin_core.exceptions import SpinAPIException
-from pyscan.drivers.thorlabs.exceptions import ThorlabsKinesisImportException
 
 
 def test_bad_imports():
