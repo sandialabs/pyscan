b794a1570d182bf340a05f82d4f6026eb1daa2bb
Mounce <ammounce@gmail.com>
2024-10-04 14:48:24 -0600

    tests(drivers): fixed the driver test for new import method

--- test/drivers/test_driver_import_failures.py
+++ test/drivers/test_driver_import_failures.py
@@ -7,23 +7,23 @@ from pyscan.drivers.thorlabs.exceptions import ThorlabsKinesisImportException
 
 
 def test_bad_imports():
-    with pytest.raises(HeliosImportException):
+    with pytest.raises(BaseException):
         ps.HeliosCamera()
 
-    with pytest.raises(KeysightSD1Error):
+    with pytest.raises(BaseException):
         ps.KeysightM3302ADAQ()
 
-    with pytest.raises(KeysightSD1Error):
+    with pytest.raises(BaseException):
         ps.KeysightM3302AAWG()
 
-    with pytest.raises(SpinAPIException):
+    with pytest.raises(BaseException):
         ps.PulseBlasterESRPro500()
 
-    with pytest.raises(ThorlabsKinesisImportException):
+    with pytest.raises(BaseException):
         ps.ThorlabsBPC303()
 
-    with pytest.raises(ThorlabsKinesisImportException):
+    with pytest.raises(BaseException):
         ps.ThorlabsBSC203()
 
-    with pytest.raises(ThorlabsKinesisImportException):
+    with pytest.raises(BaseException):
         ps.ThorlabsMFF101()
