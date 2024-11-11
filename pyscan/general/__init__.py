8ffa203fe2fb15edab087f9cbffff370eedf53ae
amounce@sandia.gov <ammounce@gmail.com>
2024-11-11 10:53:31 -0700

    chore(general): removed quadature sum, not used

--- pyscan/general/__init__.py
+++ pyscan/general/__init__.py
@@ -8,7 +8,6 @@ from .delta_product import delta_product
 from .first_string import first_string
 from .is_list_type import is_list_type
 from .is_numeric_type import is_numeric_type
-from .quadrature_sum import quadrature_sum
 from .same_length import same_length
 from .set_difference import set_difference
 from .stack_or_append import stack_or_append
