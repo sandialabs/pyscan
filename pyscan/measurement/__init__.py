7af1eb93fd68d15048f490ebdf5f28b187254184
amounce@sandia.gov <ammounce@gmail.com>
2024-11-19 08:50:46 -0700

    chore(): Removed legacy sweep

--- pyscan/measurement/__init__.py
+++ pyscan/measurement/__init__.py
@@ -3,7 +3,7 @@ from .load_experiment import load_experiment
 from .get_pyscan_version import get_pyscan_version
 
 # Scans/Experiments
-from .experiment import Sweep, Experiment
+from .experiment import Experiment
 from .scans import PropertyScan, RepeatScan, ContinuousScan, FunctionScan, AverageScan
 
 # Other objects
