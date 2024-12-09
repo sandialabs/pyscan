5273bc449b65c86e1e9560bf03ab73ac62204927
amounce@sandia.gov <ammounce@gmail.com>
2024-12-09 09:31:16 -0700

    feat(general): inifite iterator didn't work with product, integrated into delta_product

--- pyscan/general/delta_product.py
+++ pyscan/general/delta_product.py
@@ -2,14 +2,15 @@ from itertools import product
 import numpy as np
 
 
-def delta_product(iterator_list):
+def delta_product(iterator_list, continuous=False):
     '''
     Generator that yields indicies for arbitrarially long enlcosed for loops and
         a delta for each index relative to the previous indicies
 
     Parameters
     ----------
-    length_list : list(int)
+    length_list : list(range(int))
+    continuous : bool
 
     Yields
     ------
@@ -27,14 +28,37 @@ def delta_product(iterator_list):
         assert hasattr(iterable, '__iter__'), 'iterator_list contain iterable functions'
 
     iterator_list = iterator_list[::-1]
-
     last = np.zeros(len(iterator_list))
 
-    for indicies in product(*iterator_list):
-        if indicies == tuple([0 for i in range(len(iterator_list))]):
-            yield indicies, tuple([-1 for length in iterator_list])
+    if iterator_list[0] != range(1):
+        # a fully continuous scan will have range(1), otherwise, it's range(n_max)
+        # which can be handled by the product function normally
+        continuous = False
+
+    j = 0
+    while True:
+
+        if not continuous:
+            iterator = product(*iterator_list)
         else:
-            delta = np.array(indicies) - last
-            delta[np.where(delta < 0)] = -1
-            yield indicies[::-1], tuple([int(val) for val in delta])[::-1]
-        last = indicies
+            iterator = product(*(iterator_list[1:]))
+
+        for indicies in iterator:
+
+            if continuous:
+                indicies = list(indicies)
+                indicies.insert(0, j)
+                indicies = tuple(indicies)
+
+            if indicies == tuple([0 for i in range(len(iterator_list))]):
+                yield indicies, tuple([-1 for length in iterator_list])
+            else:
+                delta = np.array(indicies) - last
+                delta[np.where(delta < 0)] = -1
+                yield indicies[::-1], tuple([int(val) for val in delta])[::-1]
+            last = indicies
+
+        if continuous:
+            j += 1
+        else:
+            break
--- pyscan/general/infinite_iterator.py
+++
@@ -1,5 +0,0 @@
-def infinite_iterator():
-    i = 0
-    while True:
-        yield i
-        i += 1
