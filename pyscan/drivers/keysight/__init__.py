a3c39287b8c67a970aad0c61a3490bf59dd9c46b
Andy Mounce <amounce@sandia.gov>
2025-01-29 15:51:04 -0700

    fix(keysite driver): Added new exception for keysite SD1 loading, removed error message

--- pyscan/drivers/keysight/__init__.py
+++ pyscan/drivers/keysight/__init__.py
@@ -1,9 +1,14 @@
 import os
 
-if os.path.exists(r'c:\Program Files (x86)\Keysight\SD1\Libraries\Python'):
-    from .keysightm3302adaq import KeysightM3302ADAQ
-    from .keysightm3302aawg import KeysightM3302AAWG
-else:
+
+try:
+    if os.path.exists(r'c:\Program Files (x86)\Keysight\SD1\Libraries\Python'):
+        from .keysightm3302adaq import KeysightM3302ADAQ
+        from .keysightm3302aawg import KeysightM3302AAWG
+    else:
+        from .keysite_exceptions import KeysightSD1Error as KeysightM3302ADAQ
+        from .keysite_exceptions import KeysightSD1Error as KeysightM3302AAWG
+except FileNotFoundError:
     from .keysite_exceptions import KeysightSD1Error as KeysightM3302ADAQ
     from .keysite_exceptions import KeysightSD1Error as KeysightM3302AAWG
 
