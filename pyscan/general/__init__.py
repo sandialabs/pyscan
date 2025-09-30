be56001a804f0ecec0e9cbb9dd552814531a3b29
i-am-mounce <amounce@sandia.gov>
2025-09-30 10:30:14 -0600

    chore: removed unused files

--- pyscan/general/__init__.py
+++ pyscan/general/__init__.py
@@ -7,7 +7,6 @@ from .d_range import drange
 from .first_string import first_string
 from .is_list_type import is_list_type
 from .is_numeric_type import is_numeric_type
-from .quadrature_sum import quadrature_sum
 from .same_length import same_length
 from .set_difference import set_difference
 from .stack_or_append import stack_or_append
