25f028e71fce73c7c6ca8f91f6c64de35e812960
Dante Basile <djbasil@sandia.gov>
2026-01-15 13:44:44 -0500

    refactor optimizers module into optimization module to accommodate new functions submodule

---
+++ pyscan/optimization/functions/__init__.py
@@ -0,0 +1,2 @@
+# Global optimization example function implementations
+from .global_optimization_examples import townsend
---
+++ pyscan/optimization/functions/global_optimization_examples.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def townsend(x0, x1):
+    """
+    Townsend function to demonstrate global optimization with multiple minima.
+    https://en.wikipedia.org/wiki/Test_functions_for_optimization
+    https://botorch.org/docs/notebooks_community/clf_constrained_bo/
+    From article:
+    Townsend, Alex (January 2014). "Constrained optimization in Chebfun". chebfun.org.
+    https://www.chebfun.org/examples/opt/ConstrainedOptimization.html
+    Two local minima on domain [-2.5, 2.5], [-2.5, 2.5] subject to heart-shaped constraint.
+    Four local maxima on domain [-2.5, 2.5], [-1.5, 1.5].
+    """
+    return -np.cos((x0 - 0.1) * x1) ** 2 - x0 * np.sin(3 * x0 + x1)
