b36b09b89e282990c86f69a4fb6529fea44abd54
Dante Basile <djbasil@sandia.gov>
2026-02-16 16:26:52 -0500

    power meter property refactor

--- pyscan/drivers/thorlabs/thorlabspm16_120.py
+++ pyscan/drivers/thorlabs/thorlabspm16_120.py
@@ -2,7 +2,6 @@ from itemattribute import ItemAttribute
 from .optical_parameter_monitor.TLPMX import TLPMX, TLPM_DEFAULT_CHANNEL
 from ctypes import byref, create_string_buffer, c_bool, c_char_p, c_double, c_int, c_int16, c_uint32
 from dataclasses import dataclass
-# from time import sleep
 
 
 def string_buffer_to_str(sb):
@@ -19,7 +18,8 @@ class RsrcInfo:
 
 class ThorlabsPM16_120(ItemAttribute):
     '''
-    Docstring for Thorlabs PM16-120 USB power meter.
+    Driver for Thorlabs PM16-120 USB power meter.
+
     Parameters
     ----------
     serial : str
@@ -27,47 +27,33 @@ class ThorlabsPM16_120(ItemAttribute):
     '''
 
     def __init__(self, serial="251203529",
-                 wavelength=532.5, power_auto_range=True, power_unit=0):
+                 wavelength=532.5, power_auto_range=True, power_unit='Watt'):
 
         self._version = "0.1.0"
-        self.device_count = None
+        self.serial = serial
         self.device_list = None
         self.device_info_list = None
-        self.wavelength = None
-        self.power_auto_range = None
-        self.power_unit = None
+        self._wavelength = None
+        self._power_auto_range = None
+        self._power_unit = None
         self.connected_device_i = None
-        self.serial = serial
         self.tlPM = TLPMX()
         self.build_device_list()
         self.build_device_info_list()
-        self.disconnect()
+        self.close()
         self.open()
-        # print("Last calibration date: ", self.get_calibration_message())
-        # sleep(2)
-        self.set_wavelength(wavelength)
-        self.set_power_auto_range(power_auto_range)
-        self.set_power_unit(power_unit)
-
-    def count_devices(self):
-        # self.connect_tlPM()
-        deviceCount = c_uint32()
-        self.tlPM.findRsrc(byref(deviceCount))
-        self.device_count = deviceCount.value
+        self.wavelength = wavelength
+        self.power_auto_range = power_auto_range
+        self.power_unit = power_unit
 
     def build_device_list(self):
-        # self.connect_tlPM()
-        self.count_devices()
         self.device_list = []
         resourceName = create_string_buffer(1024)
         for i in range(self.device_count):
             self.tlPM.getRsrcName(c_int(i), resourceName)
             self.device_list.append(resourceName)
-        # self.tlPM.close()
 
     def build_device_info_list(self):
-        # self.connect_tlPM()
-        self.count_devices()
         self.device_info_list = []
         modelName = create_string_buffer(1024)
         serialNumber = create_string_buffer(1024)
@@ -80,19 +66,14 @@ class ThorlabsPM16_120(ItemAttribute):
                             string_buffer_to_str(manufacturer),
                             bool(deviceAvailable))
             self.device_info_list.append(ri_i)
-        # self.tlPM.close()
 
-    # def connect_tlPM(self):
-    #     if self.tlPM is None:
-    #         self.tlPM = TLPMX()
-
-    def disconnect(self):
+    def close(self):
         self.tlPM.close()
         self.connected_device_i = None
 
     def connect_device(self, i):
         if self.connected_device_i is not None:
-            self.disconnect()
+            self.close()
         self.tlPM = TLPMX()
         self.tlPM.open(self.device_list[i], c_bool(True), c_bool(True))
         self.connected_device_i = i
@@ -107,39 +88,91 @@ class ThorlabsPM16_120(ItemAttribute):
         if not device_opened:
             raise RuntimeError("Failed to open device")
 
-    def measure_power(self):
+    @property
+    def device_count(self):
+        deviceCount = c_uint32()
+        self.tlPM.findRsrc(byref(deviceCount))
+        return deviceCount.value
+
+    @property
+    def calibration_message(self):
+        message = create_string_buffer(1024)
+        self.tlPM.getCalibrationMsg(message, TLPM_DEFAULT_CHANNEL)
+        return string_buffer_to_str(message)
+
+    @property
+    def device_list_resource_names(self):
+        resource_name_list = []
+        for r in self.device_list:
+            resource_name_list.append(string_buffer_to_str(r))
+        return resource_name_list
+
+    @property
+    def power(self):
+        '''
+        Get current power readout
+        '''
         power = c_double()
         self.tlPM.measPower(byref(power), TLPM_DEFAULT_CHANNEL)
         return power.value
 
-    def set_wavelength(self, wavelength):
+    @property
+    def wavelength(self):
+        '''
+        Wavelength in nm
+        '''
+        return self._wavelength
+
+    @wavelength.setter
+    def wavelength(self, wavelength):
         '''
         Set wavelength in nm
         '''
         self.tlPM.setWavelength(c_double(wavelength), TLPM_DEFAULT_CHANNEL)
-        self.wavelength = wavelength
+        self._wavelength = wavelength
 
-    def set_power_auto_range(self, power_auto_range):
+    @property
+    def power_auto_range(self):
+        '''
+        Auto-range mode
+        '''
+        return self._power_auto_range
+
+    @power_auto_range.setter
+    def power_auto_range(self, power_auto_range):
         '''
         Enable or disable auto-range mode
         '''
         self.tlPM.setPowerAutoRange(c_int16(power_auto_range), TLPM_DEFAULT_CHANNEL)
-        self.power_auto_range = power_auto_range
+        self._power_auto_range = power_auto_range
 
     def set_power_unit(self, power_unit):
         '''
         0: Watt, 1: dBm
         '''
-        self.tlPM.setPowerUnit(c_int16(power_unit), TLPM_DEFAULT_CHANNEL)
-        self.power_unit = power_unit
-    
-    def get_calibration_message(self):
-        message = create_string_buffer(1024)
-        self.tlPM.getCalibrationMsg(message, TLPM_DEFAULT_CHANNEL)
-        return string_buffer_to_str(message)
+        match power_unit:
+            case 'Watt':
+                power_unit_bool = False
+            case 'dBm':
+                power_unit_bool = True
+            case _:
+                RuntimeError("power unit must be \'Watt\' or \'dBm\'")
+        self.tlPM.setPowerUnit(c_int16(power_unit_bool), TLPM_DEFAULT_CHANNEL)
 
-    def get_device_list_resource_names(self):
-        resource_name_list = []
-        for r in self.device_list:
-            resource_name_list.append(string_buffer_to_str(r))
-        return resource_name_list
+    @property
+    def power_unit(self):
+        '''
+        Power unit in Watt or dBm
+        '''
+        return self._power_unit
+
+    @power_unit.setter
+    def power_unit(self, power_unit):
+        '''
+        Set power_unit to \'Watt\' or \'dBm\'
+        '''
+        self.set_power_unit(power_unit)
+        self._power_unit = power_unit
+
+    def __del__(self):
+        self.close()
