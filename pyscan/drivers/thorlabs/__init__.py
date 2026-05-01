2c6a33fc5e96ac8048851ddb44c34ce494c85b8f
Dante Basile <djbasil@sandia.gov>
2026-05-01 15:36:32 -0400

    pyscan module import for driver test demo notebooks

--- pyscan/drivers/thorlabs/__init__.py
+++ pyscan/drivers/thorlabs/__init__.py
@@ -7,9 +7,13 @@ try:
     from .thorlabsbpc303 import ThorlabsBPC303
     from .thorlabsbsc203 import ThorlabsBSC203
     from .thorlabsmff101 import ThorlabsMFF101
+    from .thorlabsct1p import ThorlabsCT1P
+    from .thorlabskim101 import ThorlabsKIM101
     from .thorlabspm16_120 import ThorlabsPM16_120
 except ModuleNotFoundError:
     from .thorlabs_exceptions import ThorlabsKinesisImportException as ThorlabsBPC303
     from .thorlabs_exceptions import ThorlabsKinesisImportException as ThorlabsBSC203
     from .thorlabs_exceptions import ThorlabsKinesisImportException as ThorlabsMFF101
+    from .thorlabs_exceptions import ThorlabsKinesisImportException as ThorlabsCT1P
+    from .thorlabs_exceptions import ThorlabsKinesisImportException as ThorlabsKIM101
     from .thorlabs_exceptions import ThorlabsOPMImportException as ThorlabsPM16_120
