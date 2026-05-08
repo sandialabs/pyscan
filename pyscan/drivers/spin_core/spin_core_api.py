43d13152f19fa7e3d5cf3ee500630e70bd56a4c7
quantumdefects <quantumdefects@son.sandia.gov>
2026-05-07 19:58:23 -0600

    fix(driver): fixed pulse blaster driver

--- pyscan/drivers/spin_core/spin_core_api.py
+++ pyscan/drivers/spin_core/spin_core_api.py
@@ -1,11 +1,18 @@
 from itemattribute import ItemAttribute
 import ctypes
-from . import spin_api_wrapper as pb
-
+import numpy as np
 
 PULSE_PROGRAM = 0
 FREQ_REGS = 1
 
+try:
+    spinapi = ctypes.CDLL("spinapi64")
+except:
+    try:
+        spinapi = ctypes.CDLL("spinapi")
+    except:
+        print("Failed to load spinapi library.")
+        pass
 
 def enum(**enums):
     return type('Enum', (), enums)
@@ -33,66 +40,53 @@ Inst = enum(
     RTI=9
 )
 
+spinapi.pb_get_version.restype = (ctypes.c_char_p)
+spinapi.pb_get_error.restype = (ctypes.c_char_p)
+
+spinapi.pb_count_boards.restype = (ctypes.c_int)
+
+spinapi.pb_init.restype = (ctypes.c_int)
+
+spinapi.pb_select_board.argtype = (ctypes.c_int)
+spinapi.pb_select_board.restype = (ctypes.c_int)
+
+spinapi.pb_set_debug.argtype = (ctypes.c_int)
+spinapi.pb_set_debug.restype = (ctypes.c_int)
+
+spinapi.pb_set_defaults.restype = (ctypes.c_int)
+
+spinapi.pb_core_clock.argtype = (ctypes.c_double)
+spinapi.pb_core_clock.restype = (ctypes.c_int)
+
+spinapi.pb_write_register.argtype = (ctypes.c_int, ctypes.c_int)
+spinapi.pb_write_register.restype = (ctypes.c_int)
+
+spinapi.pb_start_programming.argtype = (ctypes.c_int)
+spinapi.pb_start_programming.restype = (ctypes.c_int)
+
+spinapi.pb_stop_programming.restype = (ctypes.c_int)
+
+spinapi.pb_start.restype = (ctypes.c_int)
+spinapi.pb_stop.restype = (ctypes.c_int)
+spinapi.pb_reset.restype = (ctypes.c_int)
+spinapi.pb_close.restype = (ctypes.c_int)
+
+spinapi.pb_inst_pbonly.argtype = (
+    ctypes.c_int,  # flags
+    ctypes.c_int,  # inst
+    ctypes.c_int,  # inst_data
+    ctypes.c_double  # timing value
+    )
+
+spinapi.pb_inst_pbonly.restype = (ctypes.c_int)
+
 
 class SpinCoreAPI(ItemAttribute):
-    '''
-    Driver for PulseblasterESRPro Cards
 
-    Parameters
-    ----------
-    clock : int
-        Clock frequency in MHz, defaults to 500
-    board : int
-        Index of board, defaults to 0
-
-    Methods
-    -------
-    init_pb()
-        Initializes the pluse blastert
-    get_error()
-        Returns the error code
-    count_boards()
-        Returns the number of boards in the system
-    init()
-        Initilizes the currently selected board
-    set_debug(debug)
-        Sets debug mode parameter
-    select_board(board_number)
-        Selects the board to be used
-    set_defaults()
-        Sets board defauts, bust be called when using any ohter board functions
-    core_clock(clock)
-        Sets the core clock of the currently selected board
-    write_register(address, value)
-        Writes value at register at address
-    start_programming()
-        Starts the programming of the pulse sequence
-    stop_programming()
-        Stops the progamming of the pulse sequence
-    inst(*args)
-        Adds an instruction to the program
-    start()
-        Starts running the pulse program
-    stop()
-        Stops running the pulse program
-    reset()
-        Resets the pulse program
-    close()
-        Close the connection to the pulse blaster
-    instruction(instruction)
-        Converts an instruction string to an int
-    # Multi setting methods
-    setup_single_ttl(ttl_chans, always_on_chans, total_time, ttl_time)
-        Programs a single pulse on ttl_chans for ttl_time seconds,
-        with some channels always on for total_time, seconds
-    '''
-
-    def __init__(self, clock, board=0, debug=False, **kwarg):
+    def __init__(self, clock=500, board=0, debug=False, **kwarg):
 
         for key, value in kwarg.items():
             self[key] = value
-
-        self._version = "0.1.0"
 
         self.clock = clock
         self.board = board
@@ -110,60 +104,60 @@ class SpinCoreAPI(ItemAttribute):
         self.core_clock(self.clock)
 
     def get_error(self):
-        ret = pb.get_error()
+        ret = spinapi.pb_get_error()
         return str(ctypes.c_char_p(ret).value.decode("utf-8"))
 
     def count_boards(self):
         """Return the number of boards detected in the system."""
-        return pb.count_boards()
+        return spinapi.pb_count_boards()
 
     def init(self):
         """Initialize currently selected board."""
-        return pb.init()
+        return spinapi.pb_init()
 
     def set_debug(self, debug):
-        return pb.set_debug(debug)
+        return spinapi.pb_set_debug(debug)
 
     def select_board(self, board_number):
         """Select a specific board number"""
-        return pb.select_board(board_number)
+        return spinapi.pb_select_board(board_number)
 
     def set_defaults(self):
         """
         Set board defaults. Must be called before using any other board functions.
         """
-        return pb.set_defaults()
+        return spinapi.pb_set_defaults()
 
     def core_clock(self, clock):
-        return pb.core_clock(ctypes.c_double(clock))
+        return spinapi.pb_core_clock(ctypes.c_double(clock))
 
     def write_register(self, address, value):
-        return pb.write_register(address, value)
+        return spinapi.pb_write_register(address, value)
 
     def start_programming(self):
-        return pb.start_programming(0)
+        return spinapi.pb_start_programming(0)
 
     def stop_programming(self):
-        return pb.stop_programming()
+        return spinapi.pb_stop_programming()
 
     def inst(self, *args):
         t = list(args)
-        # Argument 13 must be a double
+        #Argument 13 must be a double
         t[-1] = ctypes.c_double(t[-1])
         args = tuple(t)
-        return pb.inst_pbonly(*args)
+        return spinapi.pb_inst_pbonly(*args)
 
     def start(self):
-        return pb.start()
+        return spinapi.pb_start()
 
     def stop(self):
-        return pb.stop()
+        return spinapi.pb_stop()
 
-    def reset(self):
-        return pb.reset()
+    def reset(self): 
+        return spinapi.pb_reset()
 
     def close(self):
-        return pb.close()
+        return spinapi.pb_close()
 
     def instruction(self, instruction):
         instruction = instruction.lower()
@@ -191,14 +185,12 @@ class SpinCoreAPI(ItemAttribute):
         else:
             print('Bad instruction')
 
-    def setup_single_ttl(self,
-                         ttl_chans=[],
-                         always_on_chans=[],
-                         total_time=1,
-                         ttl_time=1e-6):
+    def setup_single_ttl(self, 
+        ttl_chans=[], always_on_chans=[], 
+        total_time=1, ttl_time=1e-6):
 
         self.stop()
-
+        
         always_on = 0
         for chan in always_on_chans:
             always_on += 2**self[chan]
@@ -209,8 +201,59 @@ class SpinCoreAPI(ItemAttribute):
 
         print(ttl, always_on)
         self.start_programming()
-        self.inst(ttl + always_on, Inst.CONTINUE, 0, ttl_time * 1e9)
-        self.inst(always_on, Inst.STOP, 0, total_time * 1e9)
+        self.inst(ttl+always_on, Inst.CONTINUE, 0, ttl_time*1e9)
+        self.inst(always_on, Inst.STOP, 0, total_time*1e9)
         self.stop_programming()
 
         self.reset()
+
+    def basler_trigger_single_frame(self, frame_rate):
+        '''
+        
+        frame_rate in 1/s
+        
+        '''
+        
+        period = 1/frame_rate*1e9
+        if period%2 == 1:
+            period += 1
+
+        trigger_camera = 2**self.camera_trigger
+
+        self.start_programming()
+        self.inst(trigger_camera, Inst.CONTINUE, 0, period)
+        self.inst(0, Inst.BRANCH, 0, period)
+        self.stop_programming()
+
+        self.reset()
+        self.start()
+
+    def basler_burst_mw(self, period, mw_settle_time):
+        '''
+        
+        frame_rate in 1/s
+        mw_settle_time in s
+        '''
+        
+        period = period*1e9
+        if period%2 == 1:
+            period += 1
+
+        mw_settle_time*=1e9
+
+        mw_on = 2**self.mw
+        trigger_camera = 2**self.camera_trigger
+        burst_on = 2**self.camera_burst
+        
+        self.start_programming()
+        self.inst(burst_on + mw_on, Inst.CONTINUE, 0, mw_settle_time)
+        self.inst(trigger_camera + mw_on, Inst.CONTINUE, 0, period)
+        self.inst(0, Inst.CONTINUE, 0, mw_settle_time)
+        self.inst(trigger_camera, Inst.BRANCH, 0, period)
+        self.stop_programming()
+
+        self.reset()
+        self.start()    
+
+
+    
